3985fea285b64e824d0bda9fdacb82b52bd3a37d
Asus <alexkara245@gmail.com>
2024-04-19 18:52:54 +0300

    ad

--- ii/ШІ.py
+++ ii/ШІ.py
@@ -1,19 +1,19 @@
 import cv2
 from PIL import Image
 
-image_path = 'dog.jpg'
+image_path = 'cat.jpeg'
 cat_face_cascade = cv2.CascadeClassifier("haarcascade_frontalcatface_extended.xml")
 image=cv2.imread(image_path)
 cat_face = cat_face_cascade.detectMultiScale(image)
 
 cat=Image.open(image_path)
-glasses = Image.open("glasses.png")
+glasses = Image.open("face.png")
 cat.convert("RGBA")
 glasses.convert("RGBA")
 for (x,y,w,h) in cat_face:
-    glasses=glasses.resize((w, int(h/3)))
-    cat.paste(glasses, (x, int(y+h/4)), glasses)
-    cat.save("dog1.png")
+    glasses=glasses.resize((w, int(h/1)))
+    cat.paste(glasses, (x, int(y+h/10)), glasses)
+    cat.save("cat_face.png")
 
-cv2.imshow("Dog", image)
+cv2.imshow("Cat", image)
 cv2.waitKey()
